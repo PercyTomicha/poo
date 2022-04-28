977a040eac1938c95f22f9dedaec77b955eecf4c
Percy Tomicha <mspercywillans@gmail.com>
2022-04-27 22:09:01 -0400

    1- Clases main() Updated

--- 1- Clases/main.dart
+++ 1- Clases/main.dart
@@ -1,10 +1,12 @@
 import 'employe.dart';
 
 main() {
+  // Instancia del Objeto de una clase - Forma #1
   Employe emp = Employe();
   emp.id = 1;
   emp.name = 'Andy';
 
+  // Instancia del Objeto de una clase - Forma #2
   Employe emp2 = Employe()
   ..id = 2
   ..name = 'AndyGeek';
